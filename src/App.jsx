b4a666afa1f72c3fd162ef5877d73f843399b004
olen-d <olen.daelhousen@outlook.com>
2023-11-22 19:15:43 -0500

    Add React Router and basic 404 page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
 import './App.css'
 
+import {
+  BrowserRouter as Router,
+  useRoutes,
+  Link,
+  Outlet,
+  useParams
+} from 'react-router-dom'
+
 import HomeView from './views/HomeView'
+
+const NoMatch = () => {
+  return(
+    <>
+      <h1>404</h1>
+      <p className='text-large'>
+        Call the Vatican. See if something is missing.
+      </p>
+      <p className='author-quote mt-n1'>
+        -- Dr. Ben Sobel
+      </p>
+    </>
+  )
+}
+const Routes = () => {
+  const element = useRoutes([
+    { path: '/', element: <HomeView />},
+    { path: '*', element: <NoMatch />}
+  ])
+  return element;
+}
 
 const App = () => {
   return (
@@ -8,7 +37,9 @@ const App = () => {
       <div className="top-logo">
         <h1 className="site-title text-medium uppercase">Light Analytics</h1>
       </div>
-      <HomeView />
+      <Router>
+        <Routes />
+      </Router>
     </>
   )
 }
