522a68ad7e9a71e44947c796aabdbb0b6045358d
Olen Daelhousen <olen@roman.search.charter.com>
2024-04-10 23:39:57 -0400

    Update style and layout for administration dashboard and charts

--- src/components/ChartColumn.jsx
+++ src/components/ChartColumn.jsx
@@ -2,8 +2,8 @@
 
 import { PropTypes } from 'prop-types'
 
-const SVG_WIDTH = 500
-const SVG_HEIGHT = 400
+const SVG_WIDTH = 300
+const SVG_HEIGHT = 200
 
 const ChartColumn = (
   {
@@ -14,11 +14,13 @@ const ChartColumn = (
     startFromValue
   }
 ) => {
-  const x0 = 100
-  const xAxisLength = SVG_WIDTH - x0 * 2
+  const x0 = 60
+  const yPaddingBottom = 60
 
-  const y0 = 100
-  const yAxisLength = SVG_HEIGHT - y0 * 2
+  const xAxisLength = SVG_WIDTH - x0
+
+  const y0 = 0
+  const yAxisLength = SVG_HEIGHT - yPaddingBottom
 
   const xAxisY = y0 +  yAxisLength
 
@@ -29,11 +31,12 @@ const ChartColumn = (
 
   const dataYRange = dataYMax - dataYMinAdjusted
 
-  const maxYTicks = 5
-
   const columnPlotWidth = xAxisLength / chartData.length
 
   const columnPadding = 4
+
+  const categoryNameOffset = categoryName.length * 3.5
+  const seriesNameOffset = seriesName.length * 3
 
   let tickInterval = 0
 
@@ -76,80 +79,83 @@ const ChartColumn = (
   }
 
   return(
-    <svg width={SVG_WIDTH} height={SVG_HEIGHT}>
-      <line
-        x1={x0 - 5}
-        y1={xAxisY}
-        x2={x0 + xAxisLength}
-        y2={xAxisY}
-        stroke='#555'
-      />
-      <text className='chart-text-label-ticks' x={x0 - 10} y={xAxisY + 4} textAnchor='end'>
-        {dataYMinAdjusted}
-      </text>
-      <text className='chart-text-label-category-name' x={(x0 + xAxisLength / 2)} y={xAxisY + 56}>
-        {categoryName}
-      </text>
-      {Array.from({ length: numYTicks }).map((element, index) => {
-        const value = (index + 1) * tickInterval
-        const yRatio = (value - dataYMinAdjusted ) / dataYRange
-        const y = y0 + (1- yRatio) * yAxisLength
-        const yValue = (index + 1)  * tickInterval
+    <div className="chart-graphic">
+      <svg width={SVG_WIDTH} height={SVG_HEIGHT}>
+        <line
+          x1={x0 - 5}
+          y1={xAxisY}
+          x2={x0 + xAxisLength}
+          y2={xAxisY}
+          stroke='#555'
+        />
+        <text className='chart-text-label-ticks' x={x0 - 10} y={xAxisY + 4} textAnchor='end'>
+          {dataYMinAdjusted}
+        </text>
+        <text className='chart-text-label-category-name' x={(x0 + xAxisLength / 2 - categoryNameOffset)} y={xAxisY + 56}>
+          {categoryName}
+        </text>
+        {Array.from({ length: numYTicks }).map((element, index) => {
+          const value = (index + 1) * tickInterval
+          const yRatio = (value - dataYMinAdjusted ) / dataYRange
+          const y = y0 + (1- yRatio) * yAxisLength
+          const yValue = (index + 1)  * tickInterval
+            return (
+              <g key={index}>
+                <line
+                  x1={x0 + xAxisLength}
+                  y1={y}
+                  x2={x0 - 5}
+                  y2={y}
+                  stroke='#555'
+                  strokeDasharray='1,1'
+                />
+                <text className='chart-text-label-ticks' x={x0 - 10} y={y + 5} textAnchor='end'>
+                  {yValue}
+                </text>
+              </g>
+            )
+          // }
+        })}
+
+        <text
+          className='chart-text-label-series-name'
+          x={20}
+          y={SVG_HEIGHT / 2 - seriesNameOffset}
+
+          textAnchor = 'middle'
+          transform={`rotate(-90, ${20}, ${SVG_HEIGHT / 2 - seriesNameOffset})`}
+        >
+          {seriesName}
+        </text>
+
+        {chartData.map(({ [categoryKey]: category, count: value }, index) => {
+          const x = x0 + index * columnPlotWidth
+          const yRatio = (value - dataYMinAdjusted ) / dataYRange
+          const y = y0 + (1 - yRatio) * yAxisLength
+          const height = yRatio * yAxisLength
+
           return (
             <g key={index}>
-              <line
-                x1={x0 + xAxisLength}
-                y1={y}
-                x2={x0 - 5}
-                y2={y}
-                stroke='#555'
-                strokeDasharray='1,1'
+              <rect
+                x={x + columnPadding / 2}
+                y={y}
+                width={columnPlotWidth - columnPadding}
+                height={height}
+                className='chart-series-1'
               />
-              <text className='chart-text-label-ticks' x={x0 - 10} y={y + 5} textAnchor='end'>
-                {yValue}
+              <text
+                className='chart-text-label-category'
+                x={x + columnPlotWidth / 2}
+                y={xAxisY + 24}
+                textAnchor='middle'
+              >
+                {category}
               </text>
             </g>
           )
-        // }
-      })}
-
-      <text
-        className='chart-text-label-series-name'
-        x={x0}
-        y={yAxisLength}
-        textAnchor = 'middle'
-        transform={`rotate(-90, ${x0 - 20}, ${yAxisLength + 20})`}
-      >
-        {seriesName}
-      </text>
-
-      {chartData.map(({ [categoryKey]: category, count: value }, index) => {
-        const x = x0 + index * columnPlotWidth
-        const yRatio = (value - dataYMinAdjusted ) / dataYRange
-        const y = y0 + (1 - yRatio) * yAxisLength
-        const height = yRatio * yAxisLength
-
-        return (
-          <g key={index}>
-            <rect
-              x={x + columnPadding / 2}
-              y={y}
-              width={columnPlotWidth - columnPadding}
-              height={height}
-              className='chart-series-1'
-            />
-            <text
-              className='chart-text-label-category'
-              x={x + columnPlotWidth / 2}
-              y={xAxisY + 24}
-              textAnchor='middle'
-            >
-              {category}
-            </text>
-          </g>
-        )
-      })}
-    </svg>
+        })}
+      </svg>
+    </div>
   )
 }
 
